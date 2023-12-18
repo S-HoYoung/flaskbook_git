a1e69d7447eb648099861d8466e4206efa10aa77
S-HoYoung <hoysong511@gmail.com>
2023-12-18 11:00:37 +0900

    Chapter 2의 추가 내용을 올립니다. (최종)

--- apps/app.py
+++ apps/app.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+from apps.config import config
 
 # SQUAlchemy 인스턴스화
 db = SQLAlchemy()
@@ -11,18 +12,21 @@ db = SQLAlchemy()
 csrf = CSRFProtect()
 
 # create_app 함수 생성
-def create_app():
+def create_app(config_key):
     # 플라스크 인스턴스(객체) 생성
     app = Flask(__name__)
-    # 앱의 config 설정
-    app.config.from_mapping(
-        SECRET_KEY='GoogleCloudPlatform',
-        SQLALCHEMY_DATABASE_URI=f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
-        SQLALCHEMY_TRACK_MODIFICATIONS=False,
-        # SQL을 콘솔 로그에 출력하는 설정
-        SQLALCHEMY_ECHO=True,
-        WTF_CSRF_SECRET_KEY='qwer1234%'
-    )
+    # config_key에 매치되는 환경의 config 클래스 읽어들임
+    app.config.from_object(config[config_key])
+
+    # # 앱의 config 설정
+    # app.config.from_mapping(
+    #     SECRET_KEY='GoogleCloudPlatform',
+    #     SQLALCHEMY_DATABASE_URI=f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
+    #     SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    #     # SQL을 콘솔 로그에 출력하는 설정
+    #     SQLALCHEMY_ECHO=True,
+    #     WTF_CSRF_SECRET_KEY='qwer1234%'
+    # )
     # SQLAlchemy와 앱 연계
     db.init_app(app)
     # CSRF와 앱 연계
